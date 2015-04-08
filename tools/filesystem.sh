8f215563a98d1652f27d7b109065b5c433158adf
avbc <avbc@users.noreply.github.com>
2015-04-08 14:53:04 -0400

    Update filesystem.sh

--- tools/filesystem.sh
+++ tools/filesystem.sh
@@ -5,7 +5,10 @@
 # ================================================
 alias minitar='tar cvpzf'
 alias miniuntar='tar -xvpzf'
- 
+
+ # rsync dry run
+alias rsyncd='rsync --dry-run'
+
 # Recursively find and remove `.DS_STORE` and `.apdisk` within the current working directory.
 function rmds {
   find . -name ".apdisk" -print0 | xargs -0 rm -Rf
@@ -56,6 +59,29 @@ function webperms {
   find ./ -type d -exec chmod 755 {} \; 
 }
 
-# rsync dry run
-alias rsyncd='rsync --dry-run'
 
+# NOTE: This uses minitar alias (above).
+# Primary purpose of this method is to dump all databases+tables, and then comrpess each colleciton of table dumps into a single database tar gz
+function mysqldumpstar {
+    DB_LIST=`mysql -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
+    for DB_NAME in $DB_LIST; do
+
+        if [[ "$DB_NAME" != "information_schema" ]] && [[ "$DB_NAME" != _* ]] ; then
+            echo "Working with database: $DB_NAME"
+
+            TABLE_LIST=`mysql $DB_NAME -N -B -e 'SHOW TABLES'`;
+            for T in $TABLE_LIST; do
+
+                DUMP_FILENAME="${DB_NAME}.${T}.sql"
+                echo "|-   Dumping $T --> ${DUMP_FILENAME}"
+
+                mysqldump --skip-comments --compact --force --opt $DB_NAME $T > $DUMP_FILENAME
+            done
+
+            TAR_FILENAME="${DB_NAME}.`date +%Y%m%d`.tar.gz"
+            echo "|- Compressing tables that were just dumped ===> ${TAR_FILENAME}"
+
+            minitar $TAR_FILENAME ${DB_NAME}.*.sql
+        fi
+    done
+}
